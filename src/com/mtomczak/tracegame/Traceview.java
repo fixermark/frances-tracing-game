e12fa9d7de288fbf429d5260193df17b1a2a0006
Mark T. Tomczak <iam@fixermark.com>
2012-05-13 10:52:20 -0400

    Traceview now draws outline object.

--- src/com/mtomczak/tracegame/Traceview.java
+++ src/com/mtomczak/tracegame/Traceview.java
@@ -8,20 +8,29 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.util.AttributeSet;
 
+import com.larvalabs.svgandroid.SVGParser;
+import com.larvalabs.svgandroid.SVG;
+
 public class Traceview extends View {
 
   int x_location_;
   int y_location_;
 
+  SVG trace_image_;
+
   public Traceview(Context context, AttributeSet attrs) {
     super(context, attrs);
     x_location_ = 30;
     y_location_ = 30;
+    trace_image_ = SVGParser.getSVGFromResource(getResources(), R.raw.smile);
+    setBackgroundColor(Color.WHITE);
   }
 
   @Override
     protected void onDraw (Canvas canvas) {
     super.onDraw(canvas);
+
+    canvas.drawPicture(trace_image_.getPicture());
 
     Paint paint = new Paint();
     paint.setColor(Color.GREEN);
